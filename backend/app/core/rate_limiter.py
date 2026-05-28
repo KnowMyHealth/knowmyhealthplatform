c6344a0c49fb5d237a7db3726634398d19c94a31
Ankush Barua <ankushbarua909@gmail.com>
2026-05-28 13:50:46 +0530

    minor import bug fix

--- backend/app/core/rate_limiter.py
+++ backend/app/core/rate_limiter.py
@@ -1,5 +1,6 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+
 from app.core.security import get_user_id_for_rate_limiting
 
-Limiter(key_func=get_user_id_for_rate_limiting)
+limiter = Limiter(key_func=get_user_id_for_rate_limiting)
